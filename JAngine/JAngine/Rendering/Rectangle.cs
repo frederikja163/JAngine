50ce9385229c5ad02c4f7ffee3c0a7ea62b99550
frederikja163 <fjaplv64@gmail.com>
2020-10-08 13:48:45 +0200

    rectangle

---
+++ JAngine/JAngine/Rendering/Rectangle.cs
@@ -0,0 +1,33 @@
+﻿namespace JAngine.Rendering
+{
+    /*
+     * Border-radius
+     * Border-width
+     * Border-color
+     *
+     * Shadow - offset
+     * Shadow - color
+     * Shadow - spread
+     *
+     *
+     * Texture - image
+     * Texture - repeat
+     * Texture - area
+     * 
+     * Color
+     * Offset - normalized
+     * Coords - pixel
+     */
+    public sealed class Rectangle : IDrawable
+    {
+        static Rectangle()
+        {
+            
+        }
+        
+        public void Draw()
+        {
+            
+        }
+    }
+}
