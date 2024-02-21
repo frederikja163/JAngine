c20313c12f4193efb1f1957b33a1c285dd30bbc3
FrederikJA <fjaplv64@gmail.com>
2024-02-21 19:42:31 +0100

    v0.2.0-beta.0

--- ECS.Benchmarks/Program.cs
+++ ECS.Benchmarks/Program.cs
@@ -4,18 +4,18 @@ using JAngine.ECS;
 
 var world = new World();
 
-for (int i = 0; i < 10_000; i++)
+for (int i = 0; i < 1_000; i++)
 {
     world.CreateEntity(i);
     world.CreateEntity(i, MathF.Log2(i));
 }
 
 Random rand = new Random(0);
-for (int i = 0; i < 10_000; i++)
+for (int i = 0; i < 1_000; i++)
 {
     using (Log.Time("Find entity", false))
     {
-        int first = world.GetComponents<int>().ToList().Find(i =>  i.Equals(rand.Next(0, 10_000)));
+        int first = world.GetComponents<int>().ToList().Find(i =>  i.Equals(rand.Next(0, 1_000)));
     }
 }
 Log.LogTimer("Find entity");
